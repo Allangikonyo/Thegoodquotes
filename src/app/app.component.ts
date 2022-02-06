ef721ca54127c29840e96823b27e81f3cf91537e
Allan Gikonyo <gikonyo257@gmail.com>
2022-02-07 00:15:29 +0300

    Initialized empty box after response is submitted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  articles: Article[]
+
+  constructor() { 
+    this.articles = [
+      new Article ('Angular', 'https//angular.io', 3)
+
+    ]
+    
+  }
   
   // getting the value of title and link in HTML, when printed out
   add_article(title: HTMLInputElement, link:HTMLInputElement){
-    console.log(title.value)
+    this.articles.push(new Article(title.value, link.value ))
+    title.value = ''
+    link.value = ''
   }
 }
